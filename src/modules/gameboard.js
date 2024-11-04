ebead0a3e00660185729d495dc55d2c7aecdd789
Gamaliel Adun <adungamaliel@gmail.com>
2024-11-04 13:00:09 +0100

    feat: update addShip method

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,21 +1,45 @@
-const Ship = require("./ship");
+const { Ship } = require("./ship");
 
 class Gameboard {
   #ships;
   #grid;
 
-  constructor() {
-    this.#grid = this.#createMatrix(10, 10);
+  constructor(size = 10) {
+    this.#grid = this.#createMatrix(size, size);
     this.#ships = [];
   }
 
-  #createMatrix(n, m, fillValue = 0) {
+  #createMatrix(n, m, fillValue = null) {
     return Array.from({ length: n }, () => Array(m).fill(fillValue));
   }
 
-  addShip(start, length, orientation) {
-    newShip = new Ship(length);
-    this.#ships.push([newShip, start, orientation]);
+  addShip(start, length, orientation = 0) {
+    let newShip = new Ship(length);
+
+    if (this.#checkPositions(start, length, orientation)) {
+      this.#ships.push(newShip);
+
+      for (let i = 0; i < length; i++) {
+        if (orientation === 0) {
+          // Horizontal Orientation
+          this.#grid[start[0]][start[1] + i] = this.#ships.length - 1;
+        } else if (orientation === 1) {
+          // Vertical Orientation
+          this.#grid[start[0] + i][start[1]] = this.#ships.length - 1;
+        }
+      }
+    }
+  }
+
+  #checkPositions(start, length, orientation) {
+    for (let i = 0; i < length; i++) {
+      if (orientation === 0) {
+        if (this.#grid[start[0]][start[1] + i]) return false;
+      } else if (orientation === 1) {
+        if (this.#grid[start[0] + i][start[1]]) return false;
+      }
+    }
+    return true;
   }
 
   get grid() {
