5090c632b5321accc4c58e511f5b8a902dbbad7e
Gamaliel Adun <adungamaliel@gmail.com>
2024-11-04 12:59:37 +0100

    test: include addShip method testing

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -4,8 +4,14 @@ let gameboard;
 
 beforeAll(() => {
   gameboard = new Gameboard();
+  gameboard.addShip([2, 2], 4, 1);
+  console.log(gameboard.grid);
 });
 
 test("Check first position", () => {
-  expect(gameboard.grid[0][0]).toBe(0);
+  expect(gameboard.grid[0][0]).toBe(null);
+});
+
+test("Check ship position", () => {
+  expect(gameboard.grid[2][2]).toBe(0);
 });
