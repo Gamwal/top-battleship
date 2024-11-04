5411617b5338b74e05dad781426e0b07081eaaa6
Gamaliel Adun <adungamaliel@gmail.com>
2024-11-04 13:21:29 +0100

    test: add test to checkPositions prevents overlapg

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -5,13 +5,26 @@ let gameboard;
 beforeAll(() => {
   gameboard = new Gameboard();
   gameboard.addShip([2, 2], 4, 1);
+  gameboard.addShip([3, 2], 4, 1);
   console.log(gameboard.grid);
 });
 
-test("Check first position", () => {
-  expect(gameboard.grid[0][0]).toBe(null);
+afterAll(() => {
+  gameboard = null;
 });
 
-test("Check ship position", () => {
-  expect(gameboard.grid[2][2]).toBe(0);
+test("Check empty position", () => {
+  expect(gameboard.grid[0][0]).toBe(null); // An empty spot
+});
+
+test("Check first ship position", () => {
+  expect(gameboard.grid[2][2]).toBe(0); // First ship should be at this position
+});
+
+test("Check last position of first ship", () => {
+  expect(gameboard.grid[5][2]).toBe(0); // Last part of the first ship
+});
+
+test("Verify non-overlapping positions", () => {
+  expect(gameboard.grid[3][3]).toBe(null); // Ensure empty cell in proximity
 });
