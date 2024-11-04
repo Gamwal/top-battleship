ee69b9f2d85e19436e098aad5eb2f7a54123b88e
Gamaliel Adun <adungamaliel@gmail.com>
2024-11-04 13:54:10 +0100

    test: add test attack and check if all ships have sunk

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -6,7 +6,6 @@ beforeAll(() => {
   gameboard = new Gameboard();
   gameboard.addShip([2, 2], 4, 1);
   gameboard.addShip([3, 2], 4, 1);
-  console.log(gameboard.grid);
 });
 
 afterAll(() => {
@@ -27,4 +26,26 @@ test("Check last position of first ship", () => {
 
 test("Verify non-overlapping positions", () => {
   expect(gameboard.grid[3][3]).toBe(null); // Ensure empty cell in proximity
+});
+
+test("Verify that ships can be attacked", () => {
+  gameboard.receiveAttack([2, 2]);
+  expect(gameboard.grid[2][2]).toBe("H");
+});
+
+test("Verify that empty grids can be attacked", () => {
+  gameboard.receiveAttack([0, 2]);
+  expect(gameboard.grid[0][2]).toBe("h");
+});
+
+test("Verify that all ships have not been sunk", () => {
+  expect(gameboard.allShipsSunk()).toBe(false);
+});
+
+test("Verify that all ships have been sunk", () => {
+  gameboard.receiveAttack([3, 2]);
+  gameboard.receiveAttack([4, 2]);
+  gameboard.receiveAttack([5, 2]);
+  gameboard.receiveAttack([6, 2]);
+  expect(gameboard.allShipsSunk()).toBe(true);
 });
