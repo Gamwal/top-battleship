4aaeb83c75dae42d09635267161a9773fbad84d0
Gamaliel Adun <adungamaliel@gmail.com>
2024-11-05 11:47:30 +0100

    feat: add functions to enable board intialization

---
+++ src/modules/utils.js
@@ -0,0 +1,54 @@
+function createGrid(grid) {
+  const nRows = grid.length;
+  const nCols = grid[0].length;
+  const gridContainer = document.createElement("div");
+  gridContainer.id = "grid-container";
+
+  for (let i = 0; i < nRows; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("row-div");
+
+    for (let j = 0; j < nCols; j++) {
+      const cell = document.createElement("div");
+
+      cell.textContent = grid[i][j];
+      cell.id = `cell_${i}_${j}`;
+      cell.classList.add("cell");
+
+      cell.addEventListener("click", (event) => {
+        console.log(event);
+      });
+
+      rowDiv.appendChild(cell);
+    }
+    gridContainer.appendChild(rowDiv);
+  }
+  return gridContainer;
+}
+
+function shipLengthGenerator() {
+  return Math.floor(Math.random() * 4 + 1);
+}
+
+function coordinateGenerator(nRows = 10, nCols = 10) {
+  const x = Math.floor(Math.random() * nRows);
+  const y = Math.floor(Math.random() * nCols);
+
+  return [x, y];
+}
+
+function randomizeOrientation() {
+  return Math.round(Math.random());
+}
+
+function addShipstoGameboard(gameboard) {
+  while (gameboard.ships.length < 10) {
+    const start = coordinateGenerator();
+    const length = shipLengthGenerator();
+    const orientation = randomizeOrientation();
+
+    gameboard.addShip(start, length, orientation);
+  }
+}
+
+module.exports = { createGrid, addShipstoGameboard };
